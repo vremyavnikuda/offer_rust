ed967576222fa561609d542f913b919cda0d9994
andrew.nevsky <andrew.nevsky@yandex.ru>
2023-09-03 20:15:22 +0700

    add task count_by

--- src/penguin_data.rs
+++ src/penguin_data.rs
@@ -29,15 +29,39 @@ pub(crate) fn penguin_data(random_text:&str){
 fn count_sheep(sheep:&[bool])-> u8 {
     sheep.iter().filter(|&&present| present).count() as u8
 }
+fn count_by(x:u32,n:u32)->Vec<u32>{
+    (1..n+1).map(|i|x*i).collect()
+}
 
 #[cfg(test)]
 mod test{
-    use crate::penguin_data::count_sheep;
+    use crate::penguin_data::{count_by, count_sheep};
 
     #[test]
     fn returns_correct_sheep_count() {
         assert_eq!(count_sheep(&[false]), 0);
         assert_eq!(count_sheep(&[true]), 1);
         assert_eq!(count_sheep(&[true, false]), 1);
+    }
+    #[test]
+    fn sample_tests() {
+        assertion(vec![1,2,3,4,5,6,7,8,9,10], (1, 10));
+        assertion(vec![2,4,6,8,10], (2, 5));
+        assertion(vec![3,6,9,12,15,18,21], (3, 7));
+        assertion(vec![50,100,150,200,250], (50, 5));
+        assertion(vec![100,200,300,400,500,600], (100, 6));
+    }
+
+    fn assertion(expected : Vec<u32>, inputs : (u32, u32)) {
+        let actual = count_by(inputs.0, inputs.1);
+
+        assert!(
+            expected == actual,
+            "\nTest failed!\n expected: [{}]\n actual: [{}]\n x: {}\n n: {}\n"
+            , expected.iter().join(", ")
+            , actual.iter().join(", ")
+            , inputs.0
+            , inputs.1
+        );
     }
 }
